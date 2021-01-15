0bb9d28fe12cc3ae5975092d9079f7d1f9712376
Infant84 <adanilevich@polaris.senacor.com>
2021-01-15 22:21:21 +0100

    corrected setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
 mkdir -p ~/.streamlit/
-echo "/
-[general]\n\
-email = \"anatoly.danilevich@mail.de\"\n\
-" > ~/.streamlit/credentials.toml
-echo "\
-[server]\n\
-headless = true\n\
-enableCORS=false\n\
-port = $PORT\n\
+
+#echo "/
+#[general]\n\
+#email = \"anatoly.danilevich@mail.de\"\n\
+#" > ~/.streamlit/credentials.toml
+echo "[server]
+headless = true
+enableCORS = false
+port = $PORT
 " > ~/.streamlit/config.toml
